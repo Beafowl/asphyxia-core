a9bcdb0fcd190ad6eb159573446e2a286acc9207
dannylin0711 <dannylin0711@gmail.com>
2023-06-25 18:14:00 +0800

    Arm fix again

--- build-arm-build.sh
+++ build-arm-build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Packing binaries"
-npx pkg ./build-env -t node16-linux-arm64 -o ./build/asphyxia-core --options no-warnings
+npx pkg ./build-env -t node16.15.0-linux-arm64 -o ./build/asphyxia-core --options no-warnings
