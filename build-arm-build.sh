2ea3dc758c8e1a533dc2974754bac2d3824d7457
dannylin0711 <dannylin0711@gmail.com>
2023-06-25 15:50:23 +0800

    Fix Github Action yml

--- build-arm-build.sh
+++ build-arm-build.sh
@@ -2,4 +2,4 @@
 
 echo "Packing binaries"
 npx pkg ./build-env -t node16-linux-armv7 -o ./build/asphyxia-core --options no-warnings
-npx pkg ./build-env -t node16-linux-arm64 -o ./build/asphyxia-core --options no-warnings
+# npx pkg ./build-env -t node16-linux-arm64 -o ./build/asphyxia-core --options no-warnings
