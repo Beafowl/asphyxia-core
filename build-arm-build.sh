33307b18041efb0928bc04d92a520ebecd3ade06
dannylin0711 <dannylin0711@gmail.com>
2023-06-26 15:59:06 +0800

    Test again

--- build-arm-build.sh
+++ build-arm-build.sh
@@ -2,6 +2,8 @@
 mkdir /root
 mkdir /root/.pkg-cache
 mkdir /root/.pkg-cache/node
+chmod 777 /root
+chmod 777 /root/.pkg-cache
 chmod 777 /root/.pkg-cache/node
 echo "Packing binaries"
 npx pkg ./build-env -t node16-linux-arm64 -o ./build/asphyxia-core --options no-warnings
