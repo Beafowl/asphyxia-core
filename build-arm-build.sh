489bf5141aad4d01d22208e3bdae6cfabb93933e
Freddie <whatis@tsdo.in>
2023-07-09 21:30:09 +0800

    Add suffix for arm

--- build-arm-build.sh
+++ build-arm-build.sh
@@ -9,7 +9,7 @@ cp ./build-env/pkg-cache/built-v16.16.0-linux-armv7 /root/.pkg-cache/built-v16.1
 chmod 777 /root/.pkg-cache/built-v16.16.0-linux-armv7
 
 echo "Packing armv7"
-npx pkg ./build-env -t node16.16.0-linux-armv7 -o ./build/asphyxia-core --options no-warnings
+npx pkg ./build-env -t node16.16.0-linux-armv7 -o ./build/asphyxia-core-armv7 --options no-warnings
 
 echo "Packing arm64"
-npx pkg ./build-env -t node16.16.0-linux-arm64 -o ./build/asphyxia-core --options no-warnings
+npx pkg ./build-env -t node16.16.0-linux-arm64 -o ./build/asphyxia-core-arm64 --options no-warnings
--- build-arm-post.sh
+++ build-arm-post.sh
@@ -2,15 +2,14 @@
 
 echo "Compressing"
 
-rm -f ./build/asphyxia-core-arm64.zip
-cd build
-zip -qq asphyxia-core-arm64.zip asphyxia-core
-cd ..
-zip -qq ./build/asphyxia-core-arm64.zip -r plugins
-
-
 rm -f ./build/asphyxia-core-armv7.zip
 cd build
-zip -qq asphyxia-core-armv7.zip asphyxia-core
+zip -qq asphyxia-core-armv7.zip asphyxia-core-armv7
 cd ..
 zip -qq ./build/asphyxia-core-armv7.zip -r plugins
+
+rm -f ./build/asphyxia-core-arm64.zip
+cd build
+zip -qq asphyxia-core-arm64.zip asphyxia-core-arm64
+cd ..
+zip -qq ./build/asphyxia-core-arm64.zip -r plugins
